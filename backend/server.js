08fb7d76ef7d6a50adb41e23244a82c8e1291eef
aboakar23-sudo <aboakar23@gmail.com>
2026-05-05 05:02:35 +0300

    Change server port from 3000 to 8080

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,5 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('HealthHub running on port ' + PORT));
